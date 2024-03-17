a99973a85bf3b231ed53e66a1e2d5e7f417ed3e9
skills nwokolo anthony <skillsnwokolo372@gmail.com>
2024-03-17 18:08:00 -0230

    token cycle changed

--- main/settings.py
+++ main/settings.py
@@ -53,8 +53,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1200),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=365),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "UPDATE_LAST_LOGIN": False,
